6e651fd95e33121de19ef87690b6f6e0c99df457
Hiroki Mizukami <6128022+piroyoung@users.noreply.github.com>
2025-06-19 10:19:09 +0900

    feature: message publisher

---
+++ src/matsuyama_jihen/util/publisher.py
@@ -0,0 +1,31 @@
+from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+from typing import List
+from azure.eventhub import EventData
+from azure.eventhub import EventHubProducerClient
+
+from .model import ExampleMessage
+
+
+class Publisher(metaclass=ABCMeta):
+    @abstractmethod
+    def publish_message(self, message: ExampleMessage) -> None:
+        pass
+
+    @abstractmethod
+    def publish_messages(self, messages: List[ExampleMessage]) -> None:
+        pass
+
+
+@dataclass(frozen=True)
+class AzureEventHubPublisher(Publisher):
+    client: EventHubProducerClient
+
+    def publish_messages(self, messages: List[ExampleMessage]) -> None:
+        event_data_batch = self.client.create_batch()
+        for message in messages:
+            event_data_batch.add(EventData(message.model_dump_json()))
+        self.client.send_batch(event_data_batch)
+
+    def publish_message(self, message: ExampleMessage) -> None:
+        self.publish_messages([message])
